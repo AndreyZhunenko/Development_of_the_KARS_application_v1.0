f363321751934861b2db2b1da1644f802eb452bd
andrey <andrey.zhunenko@mail.ru>
2021-05-21 14:31:18 +0800

    The main page of the application has been developed (v1.0)

--- app/src/main/java/com/example/main_page_of_application/MainActivity.java
+++ app/src/main/java/com/example/main_page_of_application/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.main_page_of_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView LogoCompany;
+
+    ImageButton ButtonCatalog;
+    ImageButton ButtonShops;
+    ImageButton ButtonMyprofil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
         LogoCompany = findViewById(R.id.LogoCompanyID);
         LogoCompany.setImageResource(R.drawable.main_logo);
+
+        ButtonCatalog = findViewById(R.id.btnCatalog);
+        ButtonCatalog.setImageResource(R.drawable.catalog_finish_ready);
+
+        ButtonShops = findViewById(R.id.btnShops);
+        ButtonShops.setImageResource(R.drawable.shops_in_company);
+
+        ButtonMyprofil = findViewById(R.id.btnMyProfil);
+        ButtonMyprofil.setImageResource(R.drawable.my_profil_ready);
+
     }
 }
