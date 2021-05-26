0567eaff9830f4930a419cbb054f5261ba1a753c
andrey <andrey.zhunenko@mail.ru>
2021-05-27 02:35:37 +0800

    Add ListView with categories of goods on the page Catalog (v1.0)

--- app/src/main/java/com/example/main_page_of_application/CatalogActivity.java
+++ app/src/main/java/com/example/main_page_of_application/CatalogActivity.java
@@ -6,21 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
 
 public class CatalogActivity extends Activity {
 
     ImageButton PageCatalogSearch;
     ImageButton PageCatalogLOGO;
     ImageButton PageCatalogBasket;
-
     ImageButton btn_returnBack;
-
-
     ImageButton ButtonCatalog;
     ImageButton ButtonShops;
     ImageButton ButtonMyprofil;
+
+    ListView ListCategories_of_goods;
 
 
 
@@ -28,6 +30,24 @@ public class CatalogActivity extends Activity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        ListCategories_of_goods = findViewById(R.id.ListOFcategories_id);
+        CategoryListAdapter myListCategoriesAdarter;
+        ArrayList<String>categories = new ArrayList<>();
+
+        for (int i = 0; i < 13; i++){
+            categories.add("Подарки");
+            categories.add("Посуда");
+            categories.add("Товары для дома");
+            categories.add("Игрушки");
+            categories.add("Зажигалки");
+        }
+
+        myListCategoriesAdarter = new CategoryListAdapter(this, categories);
+        ListCategories_of_goods.setAdapter(myListCategoriesAdarter);
+
+
+
 
         PageCatalogSearch = findViewById(R.id.CatalogSearch);
         PageCatalogSearch.setImageResource(R.drawable.search_1);
@@ -43,12 +63,11 @@ public class CatalogActivity extends Activity {
 
 
 
+
         ButtonCatalog = findViewById(R.id.btnCatalog);
         ButtonCatalog.setImageResource(R.drawable.catalog_finish_ready);
-
         ButtonShops = findViewById(R.id.btnShops);
         ButtonShops.setImageResource(R.drawable.shops_in_company);
-
         ButtonMyprofil = findViewById(R.id.btnMyProfil);
         ButtonMyprofil.setImageResource(R.drawable.my_profil_ready);
     }
---
+++ app/src/main/java/com/example/main_page_of_application/CategoryListAdapter.java
@@ -0,0 +1,46 @@
+package com.example.main_page_of_application;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CategoryListAdapter extends BaseAdapter {
+
+    Context ctx;
+    ArrayList<String>categories;
+    public CategoryListAdapter(Context ctx, ArrayList<String>categories){
+        this.ctx = ctx;
+        this.categories = categories;
+    }
+
+    @Override
+    public int getCount() {
+        return categories.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return categories.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String ValueCategory = categories.get(position);
+        convertView = LayoutInflater.from(ctx).inflate(R.layout.category_item, parent, false);
+        TextView NameOFcategory = convertView.findViewById(R.id.TextView_categoryItem_id);
+
+        NameOFcategory.setText(ValueCategory);
+
+        return convertView;
+    }
+}
